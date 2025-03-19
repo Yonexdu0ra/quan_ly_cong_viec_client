92f9359207d8dcf6e4be56ab54fd1ce0378e831a
yonexdu0ra <phamngocquy.7303@gmail.com>
2025-03-19 22:07:38 +0700

    cập nhật validate register

--- src/utils/validate.jsx
+++ src/utils/validate.jsx
@@ -1,21 +1,24 @@
 class Validate {
   isUsername(username) {
-    username = username.trim();
+    // username = username.trim();
     try {
-      // if (username.length < 5) {
-      //   throw new Error("Tài khoản phải có ít nhất 5 ký tự");
-      // }
-      // if (username.length > 25) {
-      //   throw new Error("Tài khoản không được quá 25 ký tự");
-      // }
-      if(username.length < 5 || username.length > 25) {
-        throw new Error(
-          "username phải có độ dài tối thiểu là 5 và tối đa là 25 ký tự và không chứa ký tự đặc biệt"
-        );
+      if (!username || username.trim() === "")
+        throw new Error("Vui lòng nhập username");
+      username = username.trim();
+      if (username.length < 5) {
+        throw new Error("username phải có độ dài tối thiểu 5 ký tự");
       }
+      if (username.length > 25) {
+        throw new Error("username có độ dài tối đa 25 ký tự");
+      }
+      // if(username.length < 5 || username.length > 25) {
+      //   throw new Error(
+      //     "username phải có độ dài tối thiểu là 5 và tối đa là 25 ký tự và không chứa ký tự đặc biệt"
+      //   );
+      // }
       const regex = /^[a-zA-Z0-9]+$/;
       if (!regex.test(username)) {
-        throw new Error("Tài khoản không được chứa ký tự đặc biệt");
+        throw new Error("username không được chứa ký tự đặc biệt");
       }
       return {
         status: true,
@@ -29,19 +32,21 @@ class Validate {
     }
   }
   isFullname(fullname) {
-    fullname = fullname.trim();
     try {
-      // if (fullname.length < 6) {
-      //   throw new Error("Họ và tên phải có ít nhất 6 ký tự");
-      // }
-      // if (fullname.length > 25) {
-      //   throw new Error("Họ và tên không được quá 25 ký tự");
-      // }
-      if(fullname.length < 6 || fullname.length > 25) {
-        throw new Error(
-          "fullname phải có độ dài tối thiểu là 5 và tối đa là 25 ký tự"
-        );
+      if (!fullname || fullname.trim() === "")
+        throw new Error("Vui lòng nhập fullname");
+      fullname = fullname.trim();
+      if (fullname.length < 6) {
+        throw new Error("fullname phải có độ dài tối thiểu 5 ký tự");
       }
+      if (fullname.length > 25) {
+        throw new Error("fullname phải có độ dài tối đa 25 ký tự");
+      }
+      // if(fullname.length < 6 || fullname.length > 25) {
+      //   throw new Error(
+      //     "fullname phải có độ dài tối thiểu là 5 và tối đa là 25 ký tự"
+      //   );
+      // }
       // const regex = /^[a-zA-Z\s]+$/;
       // if (!regex.test(fullname)) {
       //   throw new Error("Họ và tên không được chứa ký tự đặc biệt");
@@ -58,8 +63,11 @@ class Validate {
     }
   }
   isEmail(email) {
-    email = email.trim();
+    
     try {
+      if (!email || email.trim() === "")
+        throw new Error("Vui lòng nhập email");
+      email = email.trim();
       const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
       if (!regex.test(email)) {
         throw new Error("Email không hợp lệ");
@@ -76,19 +84,21 @@ class Validate {
     }
   }
   isPassword(password) {
-    password = password.trim();
     try {
-      // if (password.length < 6) {
-      //   throw new Error("Mật khẩu phải có ít nhất 6 ký tự");
-      // }
-      // if (password.length > 25) {
-      //   throw new Error("Mật khẩu không được quá 25 ký tự");
-      // }
-      if(password.length < 6 || password.length > 25) {
-        throw new Error(
-          "password phải có độ dài tối thiểu là 6 ký và tối đa là 25 ký tự"
-        );
+      if (!password || password.trim() === "")
+        throw new Error("Vui lòng nhập password");
+      password = password.trim();
+      if (password.length < 6) {
+        throw new Error("password phải có tối thiểu 6 ký tự");
       }
+      if (password.length > 25) {
+        throw new Error("password có tối đa 25 ký tự");
+      }
+      // if(password.length < 6 || password.length > 25) {
+      //   throw new Error(
+      //     "password phải có độ dài tối thiểu là 6 ký và tối đa là 25 ký tự"
+      //   );
+      // }
       return {
         status: true,
         message: "",
